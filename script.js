3ffa53a0faab666f023ee6f4820ca6ce1de9824c
Arnab-1473 <119725146+Arnab-1473@users.noreply.github.com>
2023-05-08 22:35:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let element = document.getElementById("level");
+let level = 0;
+while (element.parentNode.tagName !== "BODY") {
+  element = element.parentNode;
+  level++;
+}
+alert("The level of the element is: " + level);
+
+
